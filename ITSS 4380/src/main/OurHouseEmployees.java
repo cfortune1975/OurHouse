50e47f4f154fd8f0aa9c101cfe1b16512a8c20e2
Chad Fortune <cxf140730@utdallas.edu>
2018-04-03 22:49:40 -0500

    progress commit

--- ITSS 4380/src/main/OurHouseEmployees.java
+++ ITSS 4380/src/main/OurHouseEmployees.java
@@ -26,6 +26,9 @@ public class OurHouseEmployees {
 	private JLabel lblPhoneNumber;
 	private JLabel lblAddress;
 	private JLabel lblAddressStreetNum;
+	private JLabel lblAddressCity;
+	private JLabel lblAddressState;
+	private JLabel lblAddressZip;
 	
 	private JTextField txtNameFirst;
 	private JTextField txtNameMiddle;
@@ -72,10 +75,12 @@ public class OurHouseEmployees {
 		LoadEmployeeDB();
 
 		frmMain = new JFrame();
+		frmMain.setName("frmAdd");
+		frmMain.setFont(new Font("SansSerif", Font.PLAIN, 16));
 		frmMain.setResizable(false);
 		frmMain.getContentPane().setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 16));
 		frmMain.setTitle("Our House - Add Employee");
-		frmMain.setBounds(100, 100, 439, 354);
+		frmMain.setBounds(100, 100, 363, 385);
 		frmMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmMain.getContentPane().setLayout(null);
 
@@ -89,121 +94,146 @@ public class OurHouseEmployees {
 		frmMain.getContentPane().add(separator1);
 		
 		lblNameFirst = new JLabel("First Name:");
-		lblNameFirst.setLabelFor(txtNameFirst);
 		lblNameFirst.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 16));
 		lblNameFirst.setBounds(10, 63, 86, 20);
+		lblNameFirst.setLabelFor(txtNameFirst);
 		frmMain.getContentPane().add(lblNameFirst);
 
 		txtNameFirst = new JTextField();
 		txtNameFirst.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 16));
-		txtNameFirst.setBounds(10, 44, 97, 20);
+		txtNameFirst.setBounds(10, 38, 100, 26);
 		txtNameFirst.setColumns(10);
 		frmMain.getContentPane().add(txtNameFirst);
 		
 		lblNameMiddle = new JLabel("MI:");
-		lblNameMiddle.setLabelFor(txtNameMiddle);
 		lblNameMiddle.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		lblNameMiddle.setBounds(111, 63, 27, 20);
+		lblNameMiddle.setBounds(118, 63, 20, 20);
+		lblNameMiddle.setLabelFor(txtNameMiddle);
 		frmMain.getContentPane().add(lblNameMiddle);
 
 		txtNameMiddle = new JTextField();
 		txtNameMiddle.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 16));
-		txtNameMiddle.setBounds(112, 44, 27, 20);
+		txtNameMiddle.setBounds(115, 38, 27, 26);
 		txtNameMiddle.setColumns(2);
 		frmMain.getContentPane().add(txtNameMiddle);
 		
 		lblNameLast = new JLabel("Last Name:");
-		lblNameLast.setLabelFor(txtNameLast);
 		lblNameLast.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		lblNameLast.setBounds(149, 64, 86, 20);
+		lblNameLast.setBounds(147, 63, 86, 20);
+		lblNameLast.setLabelFor(txtNameLast);
 		frmMain.getContentPane().add(lblNameLast);
 		
 		txtNameLast = new JTextField();
 		txtNameLast.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 16));
-		txtNameLast.setBounds(149, 44, 223, 20);
+		txtNameLast.setBounds(147, 38, 200, 26);
 		txtNameLast.setColumns(20);
 		frmMain.getContentPane().add(txtNameLast);
 		
 		lblPhoneNumber = new JLabel("Phone Number:");
 		lblPhoneNumber.setFont(new Font("SansSerif", Font.PLAIN, 16));
 		lblPhoneNumber.setBounds(10, 119, 129, 20);
+		lblPhoneNumber.setLabelFor(txtPhoneArea);
 		frmMain.getContentPane().add(lblPhoneNumber);
 		
 		lblBracketOpen = new JLabel("(");
 		lblBracketOpen.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		lblBracketOpen.setBounds(10, 99, 14, 20);
+		lblBracketOpen.setBounds(10, 97, 14, 20);
+		lblBracketOpen.setLabelFor(txtPhoneArea);
 		frmMain.getContentPane().add(lblBracketOpen);
 		
 		txtPhoneArea = new JTextField();
-		lblPhoneNumber.setLabelFor(txtPhoneArea);
 		txtPhoneArea.setFont(new Font("SansSerif", Font.PLAIN, 16));
 		txtPhoneArea.setColumns(3);
-		txtPhoneArea.setBounds(18, 101, 32, 20);
+		txtPhoneArea.setBounds(18, 95, 32, 26);
 		frmMain.getContentPane().add(txtPhoneArea);
 		
 		lblBracketClose = new JLabel(")");
 		lblBracketClose.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		lblBracketClose.setBounds(52, 99, 14, 20);
+		lblBracketClose.setBounds(52, 97, 14, 20);
+		lblBracketClose.setLabelFor(txtPhoneNumber);
 		frmMain.getContentPane().add(lblBracketClose);
 		
 		txtPhoneNumber = new JTextField();
 		txtPhoneNumber.setFont(new Font("SansSerif", Font.PLAIN, 16));
 		txtPhoneNumber.setColumns(8);
-		txtPhoneNumber.setBounds(62, 101, 60, 20);
+		txtPhoneNumber.setBounds(62, 95, 76, 26);
 		frmMain.getContentPane().add(txtPhoneNumber);
 		
 		lblAddress = new JLabel("Employee Address:");
 		lblAddress.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		lblAddress.setBounds(10, 155, 142, 20);
+		lblAddress.setBounds(10, 161, 142, 20);
 		frmMain.getContentPane().add(lblAddress);
+		
+		separator2 = new JSeparator();
+		separator2.setBounds(10, 153, 320, 2);
+		frmMain.getContentPane().add(separator2);
 		
 		txtAddressStreetNum = new JTextField();
 		txtAddressStreetNum.setFont(new Font("SansSerif", Font.PLAIN, 16));
 		txtAddressStreetNum.setColumns(5);
-		txtAddressStreetNum.setBounds(10, 191, 56, 20);
+		txtAddressStreetNum.setBounds(10, 190, 56, 26);
 		frmMain.getContentPane().add(txtAddressStreetNum);
 		
 		lblAddressStreetNum = new JLabel("Street Number & Name:");
+		lblAddressStreetNum.setLabelFor(txtAddressStreetNum);
 		lblAddressStreetNum.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		lblAddressStreetNum.setBounds(10, 210, 170, 20);
+		lblAddressStreetNum.setBounds(10, 216, 170, 20);
 		frmMain.getContentPane().add(lblAddressStreetNum);
-		
-		separator2 = new JSeparator();
-		separator2.setBounds(10, 176, 200, 2);
-		frmMain.getContentPane().add(separator2);
 		
 		txtAddressStreetName = new JTextField();
 		txtAddressStreetName.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		txtAddressStreetName.setColumns(10);
-		txtAddressStreetName.setBounds(66, 191, 200, 20);
+		txtAddressStreetName.setColumns(20);
+		txtAddressStreetName.setBounds(65, 190, 200, 26);
 		frmMain.getContentPane().add(txtAddressStreetName);
 		
-		JLabel lblAddressCity = new JLabel("City:");
-		lblAddressCity.setBounds(10, 268, 42, 20);
+		txtAddressCity = new JTextField();
+		txtAddressCity.setFont(new Font("SansSerif", Font.PLAIN, 16));
+		txtAddressCity.setBounds(10, 255, 200, 26);
+		txtAddressCity.setColumns(20);
+		frmMain.getContentPane().add(txtAddressCity);
+				
+		lblAddressCity = new JLabel("City:");
+		lblAddressCity.setFont(new Font("SansSerif", Font.PLAIN, 16));
+		lblAddressCity.setBounds(10, 280, 42, 20);
+		lblAddressCity.setLabelFor(txtAddressCity);
 		frmMain.getContentPane().add(lblAddressCity);
 		
-		txtAddressCity = new JTextField();
-		txtAddressCity.setBounds(10, 243, 170, 26);
-		frmMain.getContentPane().add(txtAddressCity);
-		txtAddressCity.setColumns(20);
-		
 		txtAddressState = new JTextField();
+		txtAddressState.setFont(new Font("SansSerif", Font.PLAIN, 16));
 		txtAddressState.setColumns(2);
-		txtAddressState.setBounds(193, 243, 56, 26);
+		txtAddressState.setBounds(215, 255, 45, 26);
 		frmMain.getContentPane().add(txtAddressState);
 		
-		JLabel lblAddressState = new JLabel("State:");
-		lblAddressState.setBounds(193, 268, 42, 20);
+		lblAddressState = new JLabel("State:");
+		lblAddressState.setFont(new Font("SansSerif", Font.PLAIN, 16));
+		lblAddressState.setLabelFor(txtAddressState);
+		lblAddressState.setBounds(215, 280, 42, 20);
 		frmMain.getContentPane().add(lblAddressState);
 		
 		txtAddressZip = new JTextField();
+		txtAddressZip.setFont(new Font("SansSerif", Font.PLAIN, 16));
 		txtAddressZip.setColumns(5);
-		txtAddressZip.setBounds(258, 243, 146, 26);
+		txtAddressZip.setBounds(266, 255, 69, 26);
 		frmMain.getContentPane().add(txtAddressZip);
 		
-		JLabel lblAddressZip = new JLabel("Zip Code:");
-		lblAddressZip.setBounds(258, 268, 69, 20);
+		lblAddressZip = new JLabel("Zip Code:");
+		lblAddressZip.setFont(new Font("SansSerif", Font.PLAIN, 16));
+		lblAddressZip.setLabelFor(txtAddressZip);
+		lblAddressZip.setBounds(266, 280, 69, 20);
 		frmMain.getContentPane().add(lblAddressZip);
+		
+		JButton btnAdd = new JButton("Add");
+		btnAdd.setMnemonic('A');
+		btnAdd.setActionCommand("btnAdd");
+		btnAdd.setFont(new Font("SansSerif", Font.PLAIN, 16));
+		btnAdd.setBounds(159, 323, 89, 23);
+		frmMain.getContentPane().add(btnAdd);
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.setMnemonic('C');
+		btnCancel.setFont(new Font("SansSerif", Font.PLAIN, 16));
+		btnCancel.setBounds(258, 323, 89, 23);
+		frmMain.getContentPane().add(btnCancel);
 	}
 
 	private static int LoadEmployeeDB() 
